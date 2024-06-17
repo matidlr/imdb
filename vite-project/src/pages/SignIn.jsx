d95f6f5e7ea6e9f3c32e8fc04ec0e310337e5286
Matias <mati.delarosa1984@gmail.com>
2024-06-17 17:38:23 -0300

    sign in page

--- vite-project/src/pages/SignIn.jsx
+++ vite-project/src/pages/SignIn.jsx
@@ -1,30 +1,60 @@
 import { useState } from "react"
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
-  const [newUser, setNewUser] = useState({})
+  const [newUser, setNewUser] = useState({
+    name: 'Your name',
+    email: 'Your email',
+    password: 'Your password'
+  })
+  const navigate = useNavigate();
 
   const handleChange = (e) =>{
-    
+    setNewUser({...newUser, 
+      [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault;
+    try {
+      const {name, email, password} = newUser;
+      const res = await fetch('/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username, email, password
+        })
+      })
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <div>
       <div className="">
-        <form>
+        <form onSubmit={handleSubmit}>
            <input type='text'
                   placeholder="username"
                   onChange={handleChange}
-                  value={username}
+                  name='name'
+                  value={newUser.username}
                  />
            <input type='email'
                   placeholder="email"
                   onChange={handleChange}
-                  value={email}
+                  name='email'
+                  value={newUser.email}
                  />
            <input type='password'
                   placeholder="password"
                   onChange={handleChange}
-                  value={password}
+                  name='password'
+                  value={newUser.password}
                  />
+                 <button>Sign in</button>
         </form>
           
       </div>
