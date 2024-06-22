7a83f1e16465a2f65ee63b5805fdd86e79310c12
Matias <mati.delarosa1984@gmail.com>
2024-06-22 16:41:42 -0300

    make dashboard private

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,8 @@ import { Header } from './components/Header';
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import { PiSignIn } from 'react-icons/pi';
-
+import { PrivateRoute } from './components/PrivateRoute.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 export default function App() {
   return (
@@ -14,6 +15,9 @@ export default function App() {
          <Route path='/' element={<Home/>}/>
          <Route path='/sign-in' element={<SignIn/>}/>
          <Route path='/sign-up' element={<SignUp/>}/>
+         <Route element={<PrivateRoute/>}>
+             <Route path='/dashboard' element={<Dashboard/>} />
+         </Route>
       </Routes>
     </BrowserRouter>
       
